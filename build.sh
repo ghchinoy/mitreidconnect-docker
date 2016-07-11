6f6f874e18d785ba1ee3bb795156c7a552c8f081
Alan Crosswell <alan@crosswell.us>
2016-07-11 10:54:05 -0400

    Added ARG to select which git branch to build from. JAVA_HOME is inherited from maven. Add --build-arg to build.sh to choose the branch to build and tag with.

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-version=`curl -s https://raw.githubusercontent.com/mitreid-connect/OpenID-Connect-Java-Spring-Server/master/pom.xml \
+# Usage $0 <branch>
+# For example
+#  ./build.sh master
+#  ./build.sh 1.2.x
+# latest stable branch is 1.2.x; master is the development version.
+BRANCH=${1:-1.2.x}
+version=`curl -s https://raw.githubusercontent.com/mitreid-connect/OpenID-Connect-Java-Spring-Server/$BRANCH/pom.xml \
    | sed -n -e '1,/\<version\>/s~^.*<version>\(.*\)</version>~\1~p'`
 echo Current version of mitreid-connect is $version
-docker build --tag ghchinoy/mitreid-connect:$version --tag ghchinoy/mitreid-connect:latest -f Dockerfile.mitreid-connect .
+docker build --build-arg BRANCH=$BRANCH --tag ghchinoy/mitreid-connect:$version --tag ghchinoy/mitreid-connect:latest -f Dockerfile.mitreid-connect .
 
 
